deda1413f527ac6b631730cafa1ef9272efe63bb
Gabriel Gonçalves <gabrielgv4569@gmail.com>
2024-01-06 10:04:45 -0300

    geracao_automatica_danfe_observandopastamedia

--- src/gera_danfe.js
+++ src/gera_danfe.js
@@ -13,52 +13,96 @@ import { setProduct } from './factories/setProduct.js';
 import { setDanfeInput } from './factories/setDanfeInput.js';
 process.env.TZ = 'America/Sao_Paulo';
 
-for (let i=1; i<=100; i++)  {
+const folderPath = returnDirName();
 
-const pathDoArquivoPdf = path.join(returnDirName(), `danfe${i}.pdf`);
-const pathDoArquivoXml = path.join(returnDirName(), `arquivo (${i}).xml`);
+async function processarArquivo(filename) {
+    const filePath = path.join(folderPath, filename);
 
+    if (!fs.existsSync(filePath)) return;
 
-try {
+    console.log(`Arquivo ${filename} adicionado. Executar função...`);
     
-    const dataNf = await ConvertXmlToJson(pathDoArquivoXml)
-    const emitenteNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe ).infNFe[0].emit[0]
-    var emitente = setEmitente(emitenteNF)
+    try {
+        await generateDanfe(filePath, filename);
 
-    const destinatarioNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe ).infNFe[0].dest[0]
-    var destinatario = setDestinatario(destinatarioNF)
-
-    const transportadorNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe ).infNFe[0].transp?.[0]
-    var transportador = setTransportador(transportadorNF)
-
-    var protocolo = setProtocolo(dataNf)
-
-    const impostoNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe ).infNFe[0].total[0].ICMSTot[0]
-    var impostos = setImpostos(impostoNF)
-
-    const volumesNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe ).infNFe[0].transp?.[0].vol?.[0]
-    var volumes = setVolumes(volumesNF)
-
-    var danfeInput = setDanfeInput(dataNf, emitente, destinatario, transportador, protocolo, impostos, volumes)
-
-    const produtos = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe ).infNFe[0].det
-    setProduct(produtos, danfeInput)
-
-    new danfe.Gerador(danfeInput).gerarPDF({
-        ambiente: 'homologacao',
-        ajusteYDoLogotipo: -4,
-        ajusteYDaIdentificacaoDoEmitente: 4,
-        creditos: 'Safyra.com.br - Gestão do seu negócio!'
-    }, function (err, pdf) {
-        if (err) {
-            throw err;
-        }
-
-        pdf.pipe(fs.createWriteStream(pathDoArquivoPdf));
-        console.log("emitido com sucesso! Disponivel em: " + pathDoArquivoPdf)
-    });
-
-} catch (error) {
-    console.log(error.message)
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                console.error(`Erro ao deletar o arquivo ${filename}:`, err);
+            } else {
+                console.log(`Arquivo ${filename} deletado.`);
+            }
+        });
+    } catch (error) {
+        console.error(`Erro ao processar o arquivo ${filename}:`, error);
+    }
 }
+
+fs.watch(folderPath, async (eventType, filename) => {
+    if (eventType === 'rename' && filename.endsWith('.xml')) {
+        await processarArquivo(filename);
+    }
+});
+
+fs.readdir(folderPath, async (err, files) => {
+    if (err) {
+        console.error('Erro ao ler os arquivos existentes:', err);
+        return;
+    }
+
+    for (const file of files) {
+        if (file.endsWith('.xml')) {
+            await processarArquivo(file);
+        }
+    }
+});
+
+
+
+async function generateDanfe(filePath,filename) {
+
+    const pathDoArquivoPdf = path.join(returnDirName(), `${path.parse(filename).name}.pdf`);
+    const pathDoArquivoXml = filePath;
+
+    try {
+
+        const dataNf = await ConvertXmlToJson(pathDoArquivoXml)
+        const emitenteNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe).infNFe[0].emit[0]
+        var emitente = setEmitente(emitenteNF)
+
+        const destinatarioNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe).infNFe[0].dest[0]
+        var destinatario = setDestinatario(destinatarioNF)
+
+        const transportadorNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe).infNFe[0].transp?.[0]
+        var transportador = setTransportador(transportadorNF)
+
+        var protocolo = setProtocolo(dataNf)
+
+        const impostoNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe).infNFe[0].total[0].ICMSTot[0]
+        var impostos = setImpostos(impostoNF)
+
+        const volumesNF = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe).infNFe[0].transp?.[0].vol?.[0]
+        var volumes = setVolumes(volumesNF)
+
+        var danfeInput = setDanfeInput(dataNf, emitente, destinatario, transportador, protocolo, impostos, volumes)
+
+        const produtos = (dataNf.nfeProc?.NFe?.[0] ?? dataNf.NFe).infNFe[0].det
+        setProduct(produtos, danfeInput)
+
+        new danfe.Gerador(danfeInput).gerarPDF({
+            ambiente: 'homologacao',
+            ajusteYDoLogotipo: -4,
+            ajusteYDaIdentificacaoDoEmitente: 4,
+            creditos: 'Safyra.com.br - Gestão do seu negócio!'
+        }, function (err, pdf) {
+            if (err) {
+                throw err;
+            }
+
+            pdf.pipe(fs.createWriteStream(pathDoArquivoPdf));
+            console.log("emitido com sucesso! Disponivel em: " + pathDoArquivoPdf)
+        });
+
+    } catch (error) {
+        console.log(error.message)
+    }
 }
